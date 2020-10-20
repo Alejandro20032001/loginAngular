facc56ae0a2e9c1ac37f0fd01021267cbebbcd4d
alejandro <dilanantezana@gmail.com>
2020-10-20 11:45:10 -0400

    services implementado

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { User } from '../models/user';
+import { Jwt } from '../models/jwt';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  AUTH_SERVER: string = 'http://localhost:4200';
+  authSubject = new BehaviorSubject(false);
+  private token: string;
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  register(user: User): Observable<Jwt>{
+    return this.httpClient.post<Jwt>(`${this.AUTH_SERVER}/register`, 
+    user).pipe(tap(
+      (res: Jwt) => {
+        if(res){
+          this.saveToken(res.dataUser.accessToken, res.dataUser.expiresToken);
+        }
+      })
+    );
+  }
+
+  login(user: User): Observable<Jwt>{
+    return this.httpClient.post<Jwt>(`${this.AUTH_SERVER}/login`, 
+    user).pipe(tap(
+      (res: Jwt) => {
+        if(res){
+          this.saveToken(res.dataUser.accessToken, res.dataUser.expiresToken);
+        }
+      })
+    );
+  }
+
+  logout(){
+    this.token = null;
+    localStorage.removeItem("ACCES_TOKEN");
+    localStorage.removeItem("EXPIRES_IN");
+  }
+
+  private saveToken(token: string, expiresIn: string): void{
+    localStorage.setItem("ACCES_TOKEN", token);
+    localStorage.setItem("EXPIRES_IN", expiresIn);
+    this.token = token;
+  }
+
+  private getToken():string{
+    if(!this.token){
+      this.token = localStorage.getItem("ACEESS_TOKEN");
+    }
+    return this.token;
+  }
 }
