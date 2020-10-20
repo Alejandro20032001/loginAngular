fd100d7f7c614f0811ca21b3f2faea15fd29768c
alejandro <dilanantezana@gmail.com>
2020-10-20 11:01:02 -0400

    auth routing creado

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { from } from 'rxjs';
+
+
+const routes: Routes = [
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
