8869c43ae4bf420ec9454913c02f97760e96113c
alejandro <dilanantezana@gmail.com>
2020-10-20 11:15:04 -0400

    imports añadidos

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { from } from 'rxjs';
+
 
 
 const routes: Routes = [
