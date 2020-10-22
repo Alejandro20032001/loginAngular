8f085115b1211e7773c9ec3054049ca1875d85b4
alejandro <dilanantezana@gmail.com>
2020-10-21 20:44:33 -0400

    cambio a html y css

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -16,6 +16,7 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(form): void {
+    console.log('Login', form.value);
     this.authService.login(form.value).subscribe(res => {
       this.router.navigateByUrl('/auth');
     });
